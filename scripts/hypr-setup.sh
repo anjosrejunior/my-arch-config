570a0869a0c3c621f5cc54feb408d86042436ef6
Anjosrejunior <anjosrejuniorwork@gmail.com>
2026-08-05 20:12:27 -0300

    feat: update necessery packages

--- scripts/hypr-setup.sh
+++ scripts/hypr-setup.sh
@@ -3,6 +3,5 @@
 ## 1. Install Hyprland
 sudo pacman -S --noconfirm hyprland kitty
 
-## 2. Install necessary packages
-yay -S --noconfirm ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender libxcursor pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git hyprwire-git xcb-util-errors hyprutils-git glaze hyprgraphics-git aquamarine-git re2 hyprland-qtutils-git muparser
-sudo pacman -S --noconfirm mesa lib32-mesa xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-utils egl-wayland linux-headers linux-lts-headers
+## Drivers e Pacotes necessários
+sudo pacman -S --noconfirm mesa lib32-mesa xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-utils linux-headers linux-lts-headers 
